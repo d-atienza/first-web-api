9977b3d052797e6d006cb993b885dd9bc04caed5
d-atienza <153103146+d-atienza@users.noreply.github.com>
2024-01-29 15:02:37 +0000

    diceroll

--- app.js
+++ app.js
@@ -6,20 +6,18 @@ const app = express();
 
 app.listen(3000);
 
-function makeDBConnectionPool(dbName) {
-    //Understanding the details is not important here.
-    return new Pool({
-        database: dbName,
-    });
-}
-
-const pool = makeDBConnectionPool("omdb");
-
-// display results from db query in the browser
 app.get('/', (req, res) => {
-    pool
-    .query("select name from movies where name like 'The %' limit 10") //queries DB
-    .then((results) => { // takes results from query to db
-        res.json(results.rows); // sends result from query to browser
-    });    
-  }); 
+    res.send(diceRoll(dice));
+  });
+  
+  app.get('/about', (req, res) => {
+    res.send(diceRoll(dice));
+  });
+  
+
+let dice = [1, 2, 3, 4, 5, 6]
+
+function diceRoll(arr){
+    const index = Math.floor(Math.random() * arr.length);
+    return 'this is your current roll: ' + arr[index];
+  }
